00b57c26caf75d02412c9cb9181ca2da09649a65
clarkinada <clark.inada@hart.com>
2016-05-05 12:42:53 -0700

    fix for mulitple response added configurable http response codes/data release for 0.5.35

--- craydent.js
+++ craydent.js
@@ -255,6 +255,7 @@ if (!$g.$c || __isNewer($c.VERSION.split('.'), _craydent_version.split('.')) ) {
 		this.PRESTO = this.isPresto();
 		this.PRINCE = this.isPrince();
 		this.PROTOCOL = this.$l.protocol;
+		this.RESPONSES = $c.RESPONSES;
 		this.REST_API_TEMPLATE = $c.REST_API_TEMPLATE;
 		this.ROUTE_API_PATH = $c.ROUTE_API_PATH;
 		this.SAFARI = this.isSafari();
@@ -421,10 +422,9 @@ if (!$g.$c || __isNewer($c.VERSION.split('.'), _craydent_version.split('.')) ) {
 							}
 						}
 						if ($c.isGenerator(execute[0])) {
-							eval("$c.syncroit(function*(){yield* execute[0].call(cray, request, response, execute['v1'], setUpNext(execute.slice(1), 1));_complete();});");
+							eval("$c.syncroit(function*(){_complete(yield* execute[0].call(cray, request, response, execute['v1'], setUpNext(execute.slice(1), 1)));});");
 						} else {
-							execute[0].call(cray, request, response, execute['v1'],setUpNext(execute.slice(1), 1));
-							_complete();
+							_complete(execute[0].call(cray, request, response, execute['v1'],setUpNext(execute.slice(1), 1)));
 						}
 
 
@@ -432,7 +432,7 @@ if (!$g.$c || __isNewer($c.VERSION.split('.'), _craydent_version.split('.')) ) {
 					//}
 					function _complete() {
 						if (haveRoutes && callback == foo) {
-							return cray.send(404, {error: "no route"});
+							return cray.send(404, cray.RESPONSES["404"]);
 						}
 
 
@@ -470,14 +470,14 @@ if (!$g.$c || __isNewer($c.VERSION.split('.'), _craydent_version.split('.')) ) {
 
 						callback.call(cray, request, response);
 
-						if (!cray.DEFER_END) {
-							cray.end();
-						}
+						if (!value && !cray.DEFER_END) { cray.end(); }
+						if (value && !cray.response_sent) { cray.send(value); }
+
 					}
 				} catch (e) {
 					logit(e);
 					response.writeHead(500, header.headers);
-					response.end();
+					return cray.end(JSON.stringify(this.RESPONSES["500"]));
 					throw e;
 				} finally {
 					//            echo.out = "";
@@ -576,6 +576,81 @@ if (!$g.$c || __isNewer($c.VERSION.split('.'), _craydent_version.split('.')) ) {
 	Craydent.HIDDEN = $c.HIDDEN || "hidden";
 	Craydent.HTTP_STATUS_TEMPLATE = $c.HTTP_STATUS_TEMPLATE || [];
 	Craydent.POINTER = $c.POINTER || "default";
+	Craydent.RESPONSES = {
+		100:{"status":100,"success":true,"message":"Continue"},
+		101:{"status":101,"success":true,"message":"Swtiching Protocols"},
+		102:{"status":102,"success":true,"message":"Processing"},
+
+		200:{"status":200,"success":true,"message":"OK"},
+		201:{"status":201,"success":true,"message":"Created"},
+		202:{"status":202,"success":true,"message":"Accepted"},
+		203:{"status":203,"success":true,"message":"Non-Authoritative Information"},
+		204:{"status":204,"success":true,"message":"No Content"},
+		205:{"status":205,"success":true,"message":"Reset Content"},
+		206:{"status":206,"success":true,"message":"Partial Content"},
+		207:{"status":207,"success":true,"message":"Multi-Status"},
+		208:{"status":208,"success":true,"message":"Already Reported"},
+		226:{"status":226,"success":true,"message":"IM Used"},
+
+		300:{"status":300,"success":true,"message":"Multiple Choices"},
+		301:{"status":301,"success":true,"message":"Moved Permanently"},
+		302:{"status":302,"success":true,"message":"Found"},
+		303:{"status":303,"success":true,"message":"See Other"},
+		304:{"status":304,"success":true,"message":"Not Modified"},
+		305:{"status":305,"success":true,"message":"Use Proxy"},
+		306:{"status":306,"success":true,"message":"Unused"},
+		307:{"status":307,"success":true,"message":"Temporary Redirect"},
+		308:{"status":308,"success":true,"message":"Permanent Redirect"},
+
+		400:{"status":400,"success":false,"message":"Bad Request"},
+		401:{"status":401,"success":false,"message":"Unauthorized"},
+		402:{"status":402,"success":false,"message":"Payment Required"},
+		403:{"status":403,"success":false,"message":"Forbidden"},
+		404:{"status":404,"success":false,"message":"Not Found"},
+		405:{"status":405,"success":false,"message":"Method Not Allowed"},
+		406:{"status":406,"success":false,"message":"Not Acceptable"},
+		407:{"status":407,"success":false,"message":"Proxy Authentication Required"},
+		408:{"status":408,"success":false,"message":"Request Timeout"},
+		409:{"status":409,"success":false,"message":"Conflict"},
+		410:{"status":410,"success":false,"message":"Gone"},
+		411:{"status":411,"success":false,"message":"Length Required"},
+		412:{"status":412,"success":false,"message":"Precondition Failed"},
+		413:{"status":413,"success":false,"message":"Request Entity Too Large"},
+		414:{"status":414,"success":false,"message":"Request-URI Too Long"},
+		415:{"status":415,"success":false,"message":"Unsupported Media Type"},
+		416:{"status":416,"success":false,"message":"Requested Range Not Satisfiable"},
+		417:{"status":417,"success":false,"message":"Expectation Failed"},
+		418:{"status":418,"success":false,"message":"I'm a teapot"},
+		420:{"status":420,"success":false,"message":"Enhanced Your Calm"},
+		422:{"status":422,"success":false,"message":"Unprocessable Entity"},
+		423:{"status":423,"success":false,"message":"Locked"},
+		424:{"status":424,"success":false,"message":"Failed Dependency"},
+		425:{"status":425,"success":false,"message":"Reserved for WebDAV"},
+		426:{"status":426,"success":false,"message":"Upgrade Required"},
+		428:{"status":428,"success":false,"message":"Precondition Required"},
+		429:{"status":429,"success":false,"message":"Too Many Requests"},
+		431:{"status":431,"success":false,"message":"Request Header Fields Too Large"},
+		444:{"status":444,"success":false,"message":"No Response"},
+		449:{"status":449,"success":false,"message":"Retry With"},
+		450:{"status":450,"success":false,"message":"Blocked By Windows Parental Controls"},
+		451:{"status":451,"success":false,"message":"Unavailable For Legal Reasons"},
+		499:{"status":499,"success":false,"message":"Client Closed Request"},
+
+		500:{"status":500,"success":false,"message":"Internal Server Error"},
+		501:{"status":501,"success":false,"message":"Not Implemented"},
+		502:{"status":502,"success":false,"message":"Bad Gateway"},
+		503:{"status":503,"success":false,"message":"Service Unavailable"},
+		504:{"status":504,"success":false,"message":"Gateway Timeout"},
+		505:{"status":505,"success":false,"message":"HTTP Version Not Supported"},
+		506:{"status":506,"success":false,"message":"Variant Also Negotiates"},
+		507:{"status":507,"success":false,"message":"Insufficient Storage"},
+		508:{"status":508,"success":false,"message":"Loop Detected"},
+		509:{"status":509,"success":false,"message":"Bandwidth Limit Exceeded"},
+		510:{"status":510,"success":false,"message":"Not Extended"},
+		511:{"status":511,"success":false,"message":"Network Authentication Require"},
+		598:{"status":598,"success":false,"message":"Network read timeout error"},
+		599:{"status":599,"success":false,"message":"Network connect timeout error"}
+	};
 	Craydent.REST_API_TEMPLATE = $c.REST_API_TEMPLATE || "<html><head></head><body>" +
 		"<h1>Routes:</h1>" +
 		"<h2>All -> </h2>" +
@@ -4072,6 +4147,7 @@ function end(status, output, encoding) {
 		"url": "http://www.craydent.com/library/1.8.1/docs#emit",
 		"returnType":"(void)"
 	}|*/
+	if (this.response_sent) { return; }
 	if (status && !$c.isInt(status)) {
 		encoding = output;
 		output = status;
@@ -4107,7 +4183,7 @@ function end(status, output, encoding) {
 					eco = $c.HTTP_STATUS_TEMPLATE[404] || "<html><head></head><body><h1>"+code+": Resource Not Found</h1><p>The resource you are trying to receive was not found</p></body></html>";
 					break;
 				case ctype.indexOf('/json') != -1:
-					eco = '{"status":'+code+',"success":'+(code==200)+',"error":"Resource Not Found"}';
+					eco = JSON.stringify(this.RESPONSES["404"]);
 					break;
 			}
 
@@ -4121,11 +4197,12 @@ function end(status, output, encoding) {
 
 		!response.headersSent && response.writeHead(code, headers);
 		response.end($c.isString(output) ? pre + eco + post : output, encoding);
+		this.respond_sent = true;
 		logit('end*******************************************************');
 		//logit(echo.out);
 	} catch(e) {
 		response.writeHead(500, this.header.headers);
-		response.end($c.DEBUG_MODE ? e.toString() : "");
+		response.end($c.DEBUG_MODE ? e.toString() : JSON.stringify(this.RESPONSES["500"]));
 	} finally {
 		logit("response ended");
 	}
