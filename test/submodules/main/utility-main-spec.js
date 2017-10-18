3027a26166d6da26586207e698898e80f3899b5c
cinada <akkun1983@gmail.com>
2017-10-17 22:51:12 -0700

    completed micro unit tests

---
+++ test/submodules/main/utility-main-spec.js
@@ -0,0 +1,287 @@
+var pre = "@craydent/";
+delete global.$c;
+delete global.__craydentNoConflict;
+delete global.navigator;
+
+try { require.cache[require.resolve('../../../common.js')] && delete require.cache[require.resolve('../../../common.js')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-array')] && delete require.cache[require.resolve(pre + 'craydent-array')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-class')] && delete require.cache[require.resolve(pre + 'craydent-class')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-cli')] && delete require.cache[require.resolve(pre + 'craydent-cli')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-control-flow')] && delete require.cache[require.resolve(pre + 'craydent-control-flow')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-date')] && delete require.cache[require.resolve(pre + 'craydent-date')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-fs')] && delete require.cache[require.resolve(pre + 'craydent-fs')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-function')] && delete require.cache[require.resolve(pre + 'craydent-function')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-http')] && delete require.cache[require.resolve(pre + 'craydent-http')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-json-parser')] && delete require.cache[require.resolve(pre + 'craydent-json-parser')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-number')] && delete require.cache[require.resolve(pre + 'craydent-number')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-object')] && delete require.cache[require.resolve(pre + 'craydent-object')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-regexp')] && delete require.cache[require.resolve(pre + 'craydent-regexp')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-string')] && delete require.cache[require.resolve(pre + 'craydent-string')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-template')] && delete require.cache[require.resolve(pre + 'craydent-template')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-typeof')] && delete require.cache[require.resolve(pre + 'craydent-typeof')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-utility')] && delete require.cache[require.resolve(pre + 'craydent-utility')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-xml-to-json')] && delete require.cache[require.resolve(pre + 'craydent-xml-to-json')]; }catch(e){}
+
+try { require.cache[require.resolve(pre + 'craydent-array/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-array/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-class/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-class/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-cli/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-cli/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-control-flow/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-control-flow/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-date/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-date/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-fs/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-fs/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-function/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-function/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-http/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-http/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-json-parser/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-json-parser/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-number/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-number/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-object/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-object/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-regexp/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-regexp/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-string/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-string/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-template/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-template/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-typeof/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-typeof/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-utility/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-utility/noConflict')]; }catch(e){}
+try { require.cache[require.resolve(pre + 'craydent-xml-to-json/noConflict')] && delete require.cache[require.resolve(pre + 'craydent-xml-to-json/noConflict')]; }catch(e){}
+
+
+try { require.cache[require.resolve('../../../noConflict.js')] && delete require.cache[require.resolve('../../../noConflict.js')]; }catch(e){}
+try { require.cache[require.resolve('../../../global.js')] && delete require.cache[require.resolve('../../../global.js')]; }catch(e){}
+try { require.cache[require.resolve('../../../craydent.js')] && delete require.cache[require.resolve('../../../craydent.js')]; }catch(e){}
+var $c = require(pre + 'craydent-utility');
+var $s = require('../../../common.js');
+var $t = require(pre + 'craydent-typeof');
+$c.DEBUG_MODE = true;
+describe ('Global methods', function () {
+    beforeEach(function() {
+        this.addMatchers({
+            toMatchPropAndConstructor: function (expected) {
+                if (expected === undefined) {
+                    expected = {};
+                }
+                for (var prop in this.actual) {
+                    if (!this.actual.hasOwnProperty(prop)) { continue; }
+                    if (expected[prop] == undefined || expected[prop].constructor != this.actual[prop].constructor) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        });
+    });
+    it('ajax',function(){
+        var usersdata = { users:
+            [ { username: 'mtglass', name: 'Mark Glass', age: 10 },
+                { username: 'urdum', name: 'Ursula Dumfry', age: 10 },
+                { username: 'hydere', name: 'Henry Dere', age: 10 },
+                { username: 'cumhere', name: 'Cass Umhere', age: 10 },
+                { username: 'bstill', name: 'Bob Stillman', age: 10 },
+                { username: 'cirfuksalot', name: 'Camron', age: 10 },
+                { username: 'chadden', name: 'Corey Hadden', age: 30 },
+                { username: 'squeeb', name: 'Joseph Esquibel', age: 32 },
+                { username: 'cinada', name: 'Clark Inada', age: 31 },
+                { username: 'shurliezalot', name: 'Josh N', age: 10 },
+                { username: 'noze_nutin', name: 'Mai Boss', age: 10 },
+                { username: 'czass', name: 'Cater Zass', age: 10 },
+                { username: 'awesome_game', name: 'clash of clans', age: 21 }]};
+        $c.ajax({
+            url:"http://www.craydent.com/test/users.js",
+            onsuccess:function(data,req,ctx,status_code){
+                expect(data).toEqual(usersdata);
+                expect(status_code).toBe(200);
+            }
+        });
+
+        var errored = false, prm1 = $c.ajax({
+            url:"http://www.craydent.com/test/userss.js",
+            onsuccess:function(data,req,ctx,status_code){
+                expect('Should not execute').toBe(true);
+            },
+            onerror:function(data,req,ctx,status_code){
+                expect(status_code).toBe(404);
+                expect(errored).toBe(false);
+                errored = true;
+            }
+        });
+        prm1.otherwise(function(data,req,ctx,status_code){
+            expect(status_code).toBe(404);
+            expect(errored).toBe(true);
+        });
+
+        var stage = 1,
+            hobj = {hitch:true},
+            ctx = {thectx:true};
+        var prm2 = $c.ajax({
+            url:"http://www.craydent.com/test/users.js",
+            query:{a:"hello",b:"world"},
+            hitch:hobj,
+            context:ctx,
+            //onstatechange:function(){
+            //	expect(stage).toBe(1);
+            //	console.log(arguments,'statechange');
+            //},
+            onbefore:function(request,hitch,thiz){
+                expect(stage++).toBe(1);
+                expect(hitch).toBe(hobj);
+                expect(thiz).toBe($c);
+                expect(this).toBe(ctx);
+            },
+            oncomplete:function(data,hitch,req,status_code){
+                expect(stage++).toBe(4);
+                expect(hitch).toBe(hobj);
+                expect(data).toEqual(usersdata);
+                expect(this).toBe(ctx);
+                expect(status_code).toBe(200);
+            },
+            //onresponse:function(){
+            //	console.log(arguments,'response');
+            //},
+            //onloadstart:function(){
+            //	expect(stage++).toBe(1);
+            //	console.log(arguments,'loadstart');
+            //},
+            onsuccess:function(data,hitch,req,status_code){
+                expect(stage++).toBe(2);
+                expect(hitch).toBe(hobj);
+                expect(data).toEqual(usersdata);
+                expect(this).toBe(ctx);
+                expect(status_code).toBe(200);
+            }
+        });
+        prm2.then(function(data,req,ctx,status_code){
+            expect(stage++).toBe(3);
+            expect(data).toEqual(usersdata);
+            expect(ctx).toBe(ctx);
+            expect(status_code).toBe(200);
+        });
+
+        prm2.finally(function(data,hitch,req,status_code){
+            expect(stage++).toBe(5);
+            expect(hitch).toBe(hobj);
+            expect(data).toEqual(usersdata);
+            expect(this).toBe(ctx);
+            expect(status_code).toBe(200);
+        });
+    });
+    it('cuid',function(){
+        expect($c.cuid().length).toBe(36);
+    });
+    it('include',function(){
+        expect($c.include('../../modules/module1').toString()).toBe('function (){return "module 1"}');
+        expect($c.include('../../modules/module2').toString()).toBe('function (){return "module 2"}');
+        expect($c.include('../../modules/module3').toString()).toBe("false");
+    });
+    it('md5',function(){
+        expect($c.md5('A')).toBe('7fc56270e7a70fa81a5935b72eacbe29');
+    });
+    it('mkdirRecursive',function(){
+        $c.mkdirRecursive('/test/modules/createdModulesFolder/folder',function(err,path){
+            expect(err).toBeNull();
+            expect(path).toBe("/test/modules/createdModulesFolder/folder");
+        });
+    });
+    it('namespace',function(){
+        expect($c.namespace("Test2",function TestClass(){}).toString()).toEqual('function TestClass(){}');
+        expect($c.getFuncName((new ($c.namespace.Test2.TestClass)()).constructor)).toBe("TestClass");
+        expect(Test2).toBe('function TestClass(){}');
+    });
+    it('now',function(){
+        expect($c.now().getTime()).toBeCloseTo(new Date().getTime(),-1);
+        expect($c.now('m')).toBe(((new Date()).getMonth()+1).toString());
+    });
+    it('parseRaw',function(){
+        expect($c.parseRaw("str")).toBe("\"str\"");
+        expect($c.parseRaw({})).toBe("{}");
+        expect($c.parseRaw([])).toBe("[]");
+
+        expect($c.parseRaw("str",true)).toBe("str");
+        expect($c.parseRaw(function(){},true)).toBe("function (){}");
+        expect($c.parseRaw(function*(){},true)).toBe("function* (){}");
+    });
+    it('rand',function(){
+        var i = 0;
+        while (i < 1000) {
+            expect($c.isBetween($c.rand(1, 2, true), 1, 2, true)).toBe(true);
+            expect($c.isBetween($c.rand(1, 2), 1, 2)).toBe(true);
+            i++;
+        }
+    });
+    it('requireDirectory',function(){
+        $c.syncroit(function *() {
+            var mods = yield $c.requireDirectory('../../modules');
+            expect(mods).toMatchPropAndConstructor({'/module1.js':$c.foo,'/module2.js':$c.foo});
+
+            var mods = $c.requireDirectory('modules');
+            expect(mods).toMatchPropAndConstructor({'/module1.js':$c.foo,'/module2.js':$c.foo});
+
+            mods = $c.requireDirectory('../../modules','r');
+            expect(mods).toMatchPropAndConstructor({'/module1.js':$c.foo,'/module2.js':$c.foo,'/submodule/smodule1.js':$c.foo});
+
+            mods = $c.requireDirectory('../../modules',{recursive:true});
+            expect(mods).toMatchPropAndConstructor({'/module1.js':$c.foo,'/module2.js':$c.foo,'/submodule/smodule1.js':$c.foo});
+
+            mods = $c.requireDirectory('modules',{recursive:true});
+            expect(mods).toMatchPropAndConstructor({'/module1.js':$c.foo,'/module2.js':$c.foo,'/submodule/smodule1.js':$c.foo});
+        });
+    });
+    it('suid',function(){
+        expect($c.suid().length).toBe(10);
+        expect($c.suid(5).length).toBe(5);
+    });
+    describe("syncroit async test",function(){
+        var result = [];
+        beforeEach(function (done) {
+            $c.syncroit(function *() {
+                var resolve = true;
+
+                function testPromise(){
+                    return new Promise(function(res,rej){
+                        if (resolve) { return res({resolve:resolve}); }
+                        return rej({resolve:resolve});
+                    });
+                }
+                result.push(yield testPromise());
+
+                resolve = false;
+                result.push(yield testPromise());
+                result.push(yield $c.ajax("http://www.craydent.com/test/users.js"));
+                done();
+            });
+        });
+        it('syncroit',function(){
+            var shouldbe = [
+                {resolve: true},
+                {resolve: false},
+                {
+                    users: [{username: 'mtglass', name: 'Mark Glass', age: 10},
+                        { username: 'urdum', name: 'Ursula Dumfry', age: 10 },
+                        { username: 'hydere', name: 'Henry Dere', age: 10 },
+                        { username: 'cumhere', name: 'Cass Umhere', age: 10 },
+                        { username: 'bstill', name: 'Bob Stillman', age: 10 },
+                        { username: 'cirfuksalot', name: 'Camron', age: 10 },
+                        { username: 'chadden', name: 'Corey Hadden', age: 30 },
+                        { username: 'squeeb', name: 'Joseph Esquibel', age: 32 },
+                        { username: 'cinada', name: 'Clark Inada', age: 31 },
+                        { username: 'shurliezalot', name: 'Josh N', age: 10 },
+                        { username: 'noze_nutin', name: 'Mai Boss', age: 10 },
+                        { username: 'czass', name: 'Cater Zass', age: 10 },
+                        {username: 'awesome_game', name: 'clash of clans', age: 21}]
+                }
+            ];
+            for (var i = 0, len = result.length; i < len; i++) {
+                expect(result[i]).toEqual(shouldbe[i]);
+            }
+        });
+
+    });
+    it('tryEval',function(){
+        expect($c.tryEval("{}")).toEqual({});
+        expect($c.tryEval("{{}")).toEqual(null);
+        expect($c.tryEval("4")).toEqual(4);
+        expect($c.tryEval("\"s\"")).toEqual("s");
+    });
+    // TO/DO wait
+    //it('wait',function(){
+    //
+    //});
+    // TO/DO zipit
+    it('zipit',function(){
+    });
+
+});
