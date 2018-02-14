91ce66405014aba4172da150ee03561eb7a58768
cinada <akkun1983@gmail.com>
2018-02-13 22:41:04 -0800

    added git resolution on reject

---
+++ local_scripts/gitResolve.sh
@@ -0,0 +1,7 @@
+git pull https://github.com/craydent/node-library.git;
+git pull https://cinada@bitbucket.org/craydent/node-library.git;
+git pull https://gitlab.com/craydent/node-library.git;
+
+git pull https://cinada@github.com/craydent/Craydent-Node.git;
+git pull https://cinada@bitbucket.org/craydent/craydent-npm-library.git;
+git pull https://gitlab.com/craydent/craydent-npm-library.git;
