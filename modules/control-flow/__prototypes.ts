a95385958b2099a7b21182fad8e6be7877d66acc
cinada <akkun1983@gmail.com>
2020-06-25 12:45:58 -0700

    complete co-flo and date

---
+++ modules/control-flow/__prototypes.ts
@@ -0,0 +1,33 @@
+import * as IParallelEach from '../methods/parallelEach';
+import * as IAwaitable from '../methods/awaitable';
+import * as IYieldable from '../methods/yieldable';
+import * as ISyncroit from '../methods/syncroit';
+import { Yieldables } from '../models/Arrays';
+
+//#region dependencies
+const parallelEach: typeof IParallelEach.default = require('../methods/parallelEach');
+const awaitable: typeof IAwaitable.default = require('../methods/awaitable');
+const yieldable: typeof IYieldable.default = require('../methods/yieldable');
+const syncroit: typeof ISyncroit.default = require('../methods/syncroit');
+//#endregion
+export function _parallelEach(args: any[]): Promise<any>;
+export function _parallelEach(gen: Yieldables, args?: any[]): Promise<any>;
+export function _parallelEach(gen?, args?): Promise<any> {
+    /*|{
+        "info": "Array class extension to perform push and update indexes if used",
+        "category": "Array",
+        "featured": true,
+        "parameters":[
+            {"value": "(Object) value to add"}],
+
+        "overloads":[],
+
+        "url": "http://www.craydent.com/library/1.9.3/docs#array.parallelEach",
+        "typeParameter": "",
+        "returnType": "(Bool) Value to indicate success or failure"
+    }|*/
+    return parallelEach(this, gen, args);
+}
+
+
+export { awaitable, yieldable, syncroit };
---
+++ modules/date/__prototypes.ts
@@ -0,0 +1,64 @@
+import * as IEquals from '../methods/equals';
+import * as IFormat from '../methods/format';
+import * as IGetDayOfYear from '../methods/getDayOfYear';
+import * as IGetGMTOffset from '../methods/getGMTOffset';
+import * as IGetValue from '../methods/getValue';
+import * as IGetWeek from '../methods/getWeek';
+import * as IIsValidDate from '../methods/isValidDate';
+import * as INow from '../methods/now';
+import { AnyObject } from '../models/Arrays';
+
+//#region dependencies
+const equals: typeof IEquals.default = require('../methods/equals').default;
+const format: typeof IFormat.default = require('../methods/format').default;
+const getDayOfYear: typeof IGetDayOfYear.default = require('../methods/getDayOfYear').default;
+const getGMTOffset: typeof IGetGMTOffset.default = require('../methods/getGMTOffset').default;
+const getValue: typeof IGetValue.default = require('../methods/getValue').default;
+const getWeek: typeof IGetWeek.default = require('../methods/getWeek').default;
+const isValidDate: typeof IIsValidDate.default = require('../methods/isValidDate').default;
+const now: typeof INow.default = require('../methods/now').default;
+//#endregion
+
+export function _equals(this: AnyObject, compare: AnyObject, props?: string[]): boolean;
+export function _equals(this: any, compare: any): boolean;
+export function _equals(compare, props?): boolean {
+
+    /*|{
+        "info": "Object class extension to check if object values are equal",
+        "category": "Object",
+        "parameters":[
+            {"compare": "(any) Object to compare against"},
+            {"props?": "(String[]) Array of property values to compare against"}],
+
+        "overloads":[],
+
+        "url": "http://www.craydent.com/library/1.9.3/docs#object.equals",
+        "returnType": "(Bool)"
+    }|*/
+    return equals(this, compare, props);
+}
+
+export function _format(this: Date, formatStr: string, options?: IFormat.DateOptions): string {
+    return format(this, formatStr, options);
+}
+
+export function _getDayOfYear(this: Date): number {
+    return getDayOfYear(this);
+}
+
+export function _getGMTOffset(this: Date): number {
+    return getGMTOffset(this);
+}
+
+export function _getValue(this: Date, args?: any[], dflt?: any): any {
+    return getValue(this, args, dflt);
+}
+
+export function _getWeek(this: Date): number {
+    return getWeek(this)
+}
+export function _isValidDate(this: Date): boolean {
+    return isValidDate(this);
+}
+
+export { now };
