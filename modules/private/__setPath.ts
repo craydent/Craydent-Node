6bd5afb4c13e3bebc2a13bea9bbdcd3e05216b94
cinada <akkun1983@gmail.com>
2020-07-02 18:53:30 -0700

    __setPath test complete

--- modules/private/__setPath.ts
+++ modules/private/__setPath.ts
@@ -8,28 +8,35 @@ import isString from '../methods/isString';
 import { Route, CraydentHttp } from '../models/CraydentHttp';
 
 
-
-export default function __setPath(verb: string, http: CraydentHttp, path: string | string[], callback: Function | Function[]) {
+export type Verbs = 'delete' | 'get' | 'post' | 'put' | 'all' | 'middleware';
+export default function __setPath(verb: Verbs, http: CraydentHttp, path: string | Route | Array<string | Route>, callback?: Function | Function[]) {
     try {
-        callback = callback || [];
-        if (isFunction(callback) || isGenerator(callback) || isAsync(callback)) { callback = [callback as Function]; }
-        if (!isArray(path)) { path = [path as string]; }
-        for (let i = 0, len = path.length; i < len; i++) {
-            let route: string | Route = path[i];
+        let routes = path as any[];
+        if (!isArray(path)) { routes = [path as string]; }
+        for (let i = 0, len = routes.length; i < len; i++) {
+            let route: string | Route = routes[i];
             if (isString(route)) {
-                route = { path: route, callback, method: verb };
+                callback = callback || [];
+                route = { path: route as string, callback, method: verb };
             } else if (callback) {
                 let _route: Route = route as any;
-                _route.callback = _route.callback || [];
                 if (isFunction(_route.callback) || isGenerator(_route.callback) || isAsync(_route.callback)) {
                     _route.callback = [_route.callback as Function];
                 }
+                _route.callback = _route.callback || [];
                 _route.callback = (_route.callback as Function[]).concat(callback);
-                _route.method = verb;
+                _route.method = verb || _route.method;
             }
+            if (isFunction((route as Route).callback)
+                || isGenerator((route as Route).callback)
+                || isAsync((route as Route).callback)) {
+                (route as Route).callback = [(route as Route).callback as Function];
+            }
+            (route as Route).callback = (route as Route).callback || [];
             http.routes.push(route as any);
         }
     } catch (e) {
+        /* istanbul ignore next*/
         error && error(`CraydentServer.${verb}`, e);
     }
 }
---
+++ tests/private/__setPath.test.ts
@@ -0,0 +1,68 @@
+
+import __setPath from '../../modules/private/__setPath';
+import { CraydentHttp } from 'modules/models/CraydentHttp';
+
+describe('__setPath', () => {
+    let http: CraydentHttp;
+    beforeEach(() => {
+        http = { routes: [] } as any
+    })
+    it('should accept no callback and string as path', () => {
+        __setPath('get', http, '/path/1');
+        expect(http).toEqual({ routes: [{ method: 'get', callback: [], path: '/path/1' }] });
+    })
+    it('should accept no callback and string[] as path', () => {
+        __setPath('post', http, ['/path/2', '/path/3']);
+        expect(http).toEqual({ routes: [{ method: 'post', callback: [], path: '/path/2' }, { method: 'post', callback: [], path: '/path/3' }] });
+    })
+    it('should accept function as callback', () => {
+        let callback = () => { };
+        __setPath('put', http, '/path/4', callback);
+        expect(http).toEqual({ routes: [{ method: 'put', callback: [callback], path: '/path/4' }] });
+    })
+    it('should accept array of functions as callback', () => {
+        let callbacks = [() => { }];
+        __setPath('delete', http, ['/path/5'], callbacks);
+        expect(http).toEqual({ routes: [{ method: 'delete', callback: callbacks, path: '/path/5' }] });
+    })
+    it('should accept mixture of Route and string paths as callback', () => {
+        let callback = () => { };
+        __setPath('delete', http, ['path6', { path: 'route', callback, method: 'delete' }], callback);
+        expect(http).toEqual({
+            routes: [
+                { method: 'delete', callback: [callback], path: 'path6' },
+                { method: 'delete', callback: [callback, callback], path: 'route' }
+            ]
+        });
+    })
+    it('should accept Route as path', () => {
+        let callback = () => { };
+        __setPath('all', http, { path: 'route2', callback, method: 'get' }, callback);
+        expect(http).toEqual({ routes: [{ method: 'all', callback: [callback, callback], path: 'route2' }] });
+    })
+    it('should accept Route as path with no callback', () => {
+        let callback = () => { };
+        __setPath(null, http, { path: 'route3', callback: [callback], method: 'middleware' });
+        expect(http).toEqual({ routes: [{ method: 'middleware', callback: [callback], path: 'route3' }] });
+    })
+    it('should accept Route as path with callback and route callback is null', () => {
+        let callback = () => { };
+        __setPath(null, http, { path: 'route4', callback: null, method: 'middleware' }, callback);
+        expect(http).toEqual({ routes: [{ method: 'middleware', callback: [callback], path: 'route4' }] });
+    })
+    it('should accept Route as path with no callback and route callback is null', () => {
+        __setPath(null, http, { path: 'route5', callback: null, method: 'middleware' });
+        expect(http).toEqual({ routes: [{ method: 'middleware', callback: [], path: 'route5' }] });
+    })
+    it('should accept asyc function as callback', () => {
+        let callback = async () => { };
+        __setPath('all', http, '/path/6', callback);
+        expect(http).toEqual({ routes: [{ method: 'all', callback: [callback], path: '/path/6' }] });
+    })
+    it('should accept generator function as callback', () => {
+        let callback = function* () { };
+        __setPath('all', http, '/path/7', callback);
+        expect(http).toEqual({ routes: [{ method: 'all', callback: [callback], path: '/path/7' }] });
+    })
+
+});
